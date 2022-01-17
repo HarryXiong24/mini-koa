c8164ea53330660b10aed2f93920a7fa1ee9b921
HarryXiong24 <harryxiong24@gmail.com>
2022-01-17 17:35:16 +0800

    feat: add new content

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,49 @@
 import http from 'http';
+import Context from './context';
+import Request from './request';
+import Response from './response';
 
 export class Application {
-  public fn: (...args: any[]) => any;
+  public fn: (ctx: any, ...args: any[]) => any;
+  public context: Record<string, any>;
+  public request: Record<string, any>;
+  public response: Record<string, any>;
 
   constructor() {
     this.fn = () => {};
+    this.context = Object.create(Context);
+    this.request = Object.create(Request);
+    this.response = Object.create(Response);
   }
 
-  use(fn: any) {
+  use(fn: (ctx: any, ...args: any[]) => any) {
     this.fn = fn;
   }
 
+  createContext = (req: any, res: any) => {
+    const ctx = Object.create(this.context);
+    const request = Object.create(this.request);
+    const response = Object.create(this.response);
+    ctx.app = request.app = response.app = this;
+    ctx.request = request;
+    ctx.request.req = ctx.req = req;
+
+    ctx.response = response;
+    ctx.response.res = ctx.res = res;
+    ctx.originalUrl = request.originalUrl = req.url;
+    ctx.state = {};
+    return ctx;
+  };
+
   callback = (req: any, res: any) => {
-    this.fn(req, res);
+    const ctx = this.createContext(req, res);
+    this.fn(ctx);
+    const body = ctx.body;
+    if (body) {
+      res.end(body);
+    } else {
+      res.end('Not Found');
+    }
   };
 
   listen(...args: any[]) {
--- test/index.ts
+++ test/index.ts
@@ -2,8 +2,11 @@ import Koa from '../src/index';
 
 const app = new Koa();
 
-app.use((req: any, res: any) => {
-  res.end('Hello World\n');
+app.use((ctx: any) => {
+  // 测试 1
+  ctx.response.res.end('hello ctx.response.res');
+  // 测试 2
+  // ctx.res.end('hello ctx.res');
   console.log('Server start!');
 });
 
