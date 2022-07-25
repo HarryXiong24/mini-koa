257a478ce7730c8f08fb78b1a2431d6ac8043e42
harryxiong <harryxiong24@gmail.com>
2022-07-25 15:02:51 +0800

    fix: fix some bugs

--- src/core/application.ts
+++ src/core/application.ts
@@ -1,17 +1,20 @@
 import http from 'http';
+import EventEmitter from 'events';
 import { compose } from './compose';
 import context from '../common/context';
 import request from '../common/request';
 import response from '../common/response';
 import { Context, Middleware, Request, Response } from '../types';
+import { Stream } from 'stream';
 
-export class Application {
+export class Application extends EventEmitter {
   public middleware: Middleware;
   public context: Context;
   public request: Request;
   public response: Response;
 
   constructor() {
+    super();
     this.middleware = [];
     this.context = Object.create(context);
     this.request = Object.create(request);
@@ -53,17 +56,38 @@ export class Application {
   /**
    * 控制中间件的总回调方法
    */
-  callback = (req: any, res: any) => {
+  handleRequest = (req: any, res: any) => {
+    res.statusCode = 404;
     const ctx = this.createContext(req, res);
     const middleware = this.middleware;
-    const body = ctx.body;
-    if (body) {
-      res.end(body);
-    } else {
-      res.end('Not Found');
-    }
+    // const body = ctx.body;
+    // if (body) {
+    //   res.end(body);
+    // } else {
+    //   res.end('Not Found');
+    // }
     // 执行中间件
-    compose(middleware)(ctx).catch((err) => this.onerror(err));
+    // compose(middleware)(ctx).catch((err) => this.onerror(err));
+    const fn = compose(middleware);
+    fn(ctx)
+      .then(() => {
+        if (typeof ctx.body == 'object') {
+          res.setHeader('Content-Type', 'application/json;charset=utf8');
+          res.end(JSON.stringify(ctx.body));
+        } else if (ctx.body instanceof Stream) {
+          ctx.body.pipe(res);
+        } else if (typeof ctx.body === 'string' || Buffer.isBuffer(ctx.body)) {
+          res.setHeader('Content-Type', 'text/htmlcharset=utf8');
+          res.end(ctx.body);
+        } else {
+          res.end('Not found');
+        }
+      })
+      .catch((err) => {
+        this.emit('error', err);
+        res.statusCode = 500;
+        res.end('server error');
+      });
   };
 
   /**
@@ -71,17 +95,9 @@ export class Application {
    * @param {*} args
    */
   listen(...args: any[]) {
-    const server = http.createServer(this.callback);
+    const server = http.createServer(this.handleRequest.bind(this));
     // console.log(...args);
     server.listen(...args);
-  }
-
-  /**
-   * 异常处理监听
-   * @param {EndOfStreamError} err
-   */
-  onerror(err: Error) {
-    console.log(err);
   }
 }
 
